1aa7d7717b99cefbf5c031928cf523fc85146905
drzenit <starikovandru@gmail.com>
2020-04-16 22:14:03 +0300

    done uniformDist without graph

--- lab2/commonFunc.py
+++ lab2/commonFunc.py
@@ -1,6 +1,4 @@
 import random
-import matplotlib.pyplot as plt
-from scipy.stats import uniform
 
 
 # Генерация n случайных вещественных чисел [0,1]
@@ -10,13 +8,3 @@ def generateRandomList(n):
         randNumbList.append(random.random())
 
     return randNumbList
-
-# Построение функции плотности распределения
-def graphProbabilityDensity(numbList):
-    plt.plot(numbList, uniform.pdf(numbList))
-    plt.show()
-
-# Построение интегральной функции распределения
-def graphIntegralProbabilityDensity(numbList):
-    plt.plot(numbList, uniform.cdf(numbList))
-    plt.show()
---
+++ lab2/uniformDist.py
@@ -0,0 +1,55 @@
+from prettytable import PrettyTable
+
+from lab2 import commonFunc
+
+
+class UniformDist:
+    def __init__(self, n: int, lowLimit: int, upLimit: int):
+        self.n = n
+        self.numbList = commonFunc.generateRandomList(self.n)
+        self.lowLimit = lowLimit
+        self.upLimit = upLimit
+
+    def IRNUNI(self, ILOW: int, IUP: int, floatNum: float):
+        r = (IUP - ILOW + 1) * floatNum + ILOW
+
+        return round(r)
+
+    def createIntList(self):
+        intNumbList = list()
+        for i in self.numbList:
+            IR = self.IRNUNI(self.lowLimit, self.upLimit, i)
+            intNumbList.append(IR)
+
+        return intNumbList
+
+    def getMathematicalExpectation(self, numbList: list, n: int):
+        sumOfNumb = 0.0
+        for i in numbList:
+            sumOfNumb += i
+        matExpec = sumOfNumb / (n - 1)
+
+        return matExpec
+
+    def getDispersion(self, numbList: list, matExpec: float, n: int):
+        deviation = 0.0
+        for i in numbList:
+            deviation += ((i - matExpec) ** 2)
+        dispersion = deviation / (n - 1)
+
+        return dispersion
+
+    def outputResult(self):
+        resultTable = PrettyTable()
+        resultTable.field_names = ["Оценка", "IRNUNI", "Погрешность", "Теоретическое значение"]
+        numbList = self.createIntList()
+        matExpec = self.getMathematicalExpectation(numbList, self.n)
+        dispersion = self.getDispersion(numbList, matExpec, self.n)
+
+        resultTable.add_row(["M = \n D = ", "%f\n%f" % (matExpec, dispersion), "%f\n%f" % ((50.5 - matExpec), (833.25 - dispersion)), "%f\n%f" % (50.5, 833.25)])
+        print(resultTable)
+
+
+uniformDist = UniformDist(10000, 1, 100)
+
+uniformDist.outputResult()
