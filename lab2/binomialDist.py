f064c38c01a84015374687113bb5fc1a2c1c0ceb
drzenit <starikovandru@gmail.com>
2020-04-19 00:47:22 +0300

    creat init binomialDist

--- lab2/binomialDist.py
+++ lab2/binomialDist.py
@@ -2,5 +2,14 @@ from lab2 import commonFunc
 
 
 class BinomialDist():
-    def __init__(self):
-        pass
+    def __init__(self, n: int, N: int, p: float):
+        self.n = n
+        self.numbList = commonFunc.generateRandomList(self.n)
+        self.N = N
+        self.p = p
+        for i in self.numbList:
+            self.binomList = commonFunc.IRNUNI(0, self.N, i)
+
+a = BinomialDist(1000, 10, 0.5)
+
+print(a.binomList)
