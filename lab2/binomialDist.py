e744cb20dcdfc5226170ce9d294a45c53a79b574
drzenit <starikovandru@gmail.com>
2020-04-21 21:05:53 +0300

    done binomialDist without checks and all  graph

--- lab2/binomialDist.py
+++ lab2/binomialDist.py
@@ -1,5 +1,4 @@
-import random
-
+from math import factorial
 import sns as sns
 
 from lab2 import commonFunc
@@ -19,6 +18,61 @@ class BinomialDist():
         self.binomList = list()
         for i in self.numbList:
             self.binomList.append(commonFunc.IRNUNI(0, self.N - 1, i))
+
+    def bernFunc(self):
+        listP = list()
+        for r in range(11):
+            bernForm = (factorial(self.N) / (factorial(r) * factorial(self.N - r))) * (self.p**r) * ((1 - self.p)**(self.N - r))
+            listP.append(bernForm)
+
+        return listP
+
+    def binomFunc(self):
+        p0 = (1 - self.p)**self.N
+        p = p0
+        listP = list()
+        listP.append(p)
+        for r in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+            binomForm = p * (((self.N - r) / (r + 1)) * (self.p / (1 - self.p)))
+            listP.append(binomForm)
+            p = binomForm
+
+        return listP
+
+    def IRNBNL(self, randNum: float):
+        P = self.bernFunc()
+        M = randNum
+        for x in range(11):
+            M = M - P[x]
+            if (M >= 0):
+                continue
+            elif (M < 0):
+                return x
+
+    def IRNBIN(self, randNum: float):
+        P = self.binomFunc()
+        M = randNum
+        for x in range(11):
+            M = M - P[x]
+            if (M >= 0):
+                continue
+            elif (M < 0):
+                return x
+
+    def createBinomList(self, method: str):
+        binomList = list()
+        if (method == "BNL"):
+            for i in self.numbList:
+                IR = self.IRNBNL(i)
+                if (IR != None):
+                    binomList.append(IR)
+        elif (method == "BIN"):
+            for i in self.numbList:
+                IR = self.IRNBIN(i)
+                if (IR != None):
+                    binomList.append(IR)
+
+        return binomList
 
     def findMaxFrq(self):
         maxNumbList = list()
@@ -46,13 +100,14 @@ class BinomialDist():
 
 a = BinomialDist(10000, 10, 0.5)
 
-print(a.binomList)
-
-#print(a.findMaxFrq())
+print(a.binomFunc())
+print(a.bernFunc())
+print(a.createBinomList("BNL"))
+print(len(a.createBinomList("BNL")))
 
 #  Практика
 fig, ax = plt.subplots(figsize=(14,7))
-sns.distplot(a.binomList, bins=11, label='simulation results')
+sns.distplot(a.createBinomList("BNL"), bins=11, label='simulation results')
 ax.set_xlabel("Number of Heads",fontsize=16)
 ax.set_ylabel("Frequency",fontsize=16)
 
@@ -63,16 +118,4 @@ ax.plot(x, binom.pmf(x, 10, 0.5), 'ro', label='actual binomial distribution')
 ax.vlines(x, 0, binom.pmf(x, 10, 0.5), colors='r', lw=5, alpha=0.5)
 plt.legend()
 plt.show()
-
-
-def testFunc(N: int, p = 0.5):
-    a0 = (1 - p)**N
-    b = a0
-    listN = list()
-    listN.append(a0)
-    for r in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
-        a = b * (((N-r)/(r+1))*(p/(1-p)))
-        listN.append(a)
-        b = a
-    return listN
 
