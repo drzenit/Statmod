b55645b1907880f3a66e85bd9965d2ff874d13da
drzenit <starikovandru@gmail.com>
2020-04-27 17:49:05 +0300

    some fixex in lab2

--- lab2/labTwo.py
+++ lab2/labTwo.py
@@ -1,4 +1,4 @@
-from lab2 import binomialDist, dist, geometricDist, poissonDist, logarithmicDist
+from lab2 import binomialDist, uniformDist, geometricDist, poissonDist, logarithmicDist
 
 
 n = 10000
@@ -6,17 +6,17 @@ n = 10000
 print("ДИСКРЕТНЫЕ РАСПРЕДЕЛЕНИЯ \n\n")
 print("Выберите распределение (напечатайте нужную цифру):\n")
 
-distChoice = input("1 - Равномерное распределение; \n 2 - Биномиальное распределение; \n 3 - Геометрическое распределение;"
+distChoice = input(" 1 - Равномерное распределение; \n 2 - Биномиальное распределение; \n 3 - Геометрическое распределение;"
       "\n 4 - Пуассоновское распределение; \n 5 - Логарифмическое распределение;\n\n")
 
-if (distChoice == 1):
-    dist = dist.UniformDist(n, 1, 100)
+if (distChoice == '1'):
+    dist = uniformDist.UniformDist(n, 1, 100)
     print("Равномерное распределение:")
     dist.outputResult()
     dist.graphSimulationResult()
     dist.graphProbabilityDensity()
     dist.graphIntegralProbabilityDensity()
-elif (distChoice == 2):
+elif (distChoice == '2'):
     dist1 = binomialDist.BinomialDist(n, 10, 0.5, "BNL")
     dist2 = binomialDist.BinomialDist(n, 10, 0.5, "BIN")
     print("Биномиальное распределение:")
@@ -25,7 +25,7 @@ elif (distChoice == 2):
     dist2.graphSimulationResult()
     dist1.graphProbabilityDensity()
     dist1.graphIntegralProbabilityDensity()
-elif (distChoice == 3):
+elif (distChoice == '3'):
     dist1 = geometricDist.GeometricDist(n, 0.5, "1")
     dist2 = geometricDist.GeometricDist(n, 0.5, "2")
     dist3 = geometricDist.GeometricDist(n, 0.5, "3")
@@ -36,7 +36,7 @@ elif (distChoice == 3):
     dist3.graphSimulationResult()
     dist1.graphProbabilityDensity()
     dist1.graphIntegralProbabilityDensity()
-elif (distChoice == 4):
+elif (distChoice == '4'):
     dist1 = poissonDist.PoissonDist(n, 10, "PSN")
     dist2 = poissonDist.PoissonDist(n, 10, "SPEC")
     print("Пуассоновское распределение:")
@@ -45,7 +45,7 @@ elif (distChoice == 4):
     dist2.graphSimulationResult()
     dist1.graphProbabilityDensity()
     dist1.graphIntegralProbabilityDensity()
-elif (distChoice == 5):
+elif (distChoice == '5'):
     dist = logarithmicDist.LogarithmicDist(n, 0.5)
     print("Логарифмическое распределение:")
     dist.outputResult()
